55b446276c68a63007cfea53188655c965747f64
Matthew Davenport <madavenport94@gmail.com>
2020-11-03 10:32:41 -0500

    add logic file.

---
+++ controlling_program_logic_and_flow.js
@@ -0,0 +1,19 @@
+// Boolean Logic
+
+
+// Comparisons
+/*
+> - greater than
+< - less than
+>= - greater than or equal to
+<= - less than or equal to
+== - equality
+!= - not equal
+=== - strict equality
+!== - strict non-equality
+*/
+
+console.log(-2 > 1);
+console.log(10 < 999);
+console.log(55.1 <= 55.5);
+console.log("hello".length <= "hello!".length);
